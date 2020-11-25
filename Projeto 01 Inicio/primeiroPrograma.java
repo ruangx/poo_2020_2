aeae7c9c0d8f1f7fced2516b29ad3b8f6bf1b909
ruangx <74931475+ruangx@users.noreply.github.com>
2020-11-25 09:26:59 -0300

    Create primeiroPrograma.java

---
+++ Projeto 01 Inicio/primeiroPrograma.java
@@ -0,0 +1,8 @@
+public class primeiroPrograma{
+public static void main(String[] args) {
+    for (int i = 150; i <=300; i = i+1){
+        System.out.printf("O número da vez é = %d\n",(i));
+    }
+}
+}
+
