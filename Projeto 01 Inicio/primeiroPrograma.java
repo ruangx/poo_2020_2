7bbfd425cb1b2461aa38efc121160fd625c8ec77
ruangx <74931475+ruangx@users.noreply.github.com>
2020-11-26 09:21:30 -0300

    Update primeiroPrograma.java

--- Projeto 01 Inicio/primeiroPrograma.java
+++ Projeto 01 Inicio/primeiroPrograma.java
@@ -1,7 +1,7 @@
 public class primeiroPrograma{
 public static void main(String[] args) {
     for (int i = 150; i <=300; i = i+1){
-        System.out.printf("O número da vez é = %d\n",(i));
+        System.out.printf("O numero da vez e = %d\n",(i));
     }
 }
 }
